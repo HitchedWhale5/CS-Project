9d9c55e82852e6c2b9920ddfe4ac659c198a4b6c
HitchedWhale5 <79484996+HitchedWhale5@users.noreply.github.com>
2023-04-20 09:31:07 -0500

    Update Company.java

--- Company.java
+++ Company.java
@@ -3,96 +3,73 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+import javax.print.attribute.standard.Media;
+
 public class Company {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws SQLException {
 		System.out.println("Booting up...");
 		
 		Database db = new Database();
 		db.connect();
 		
-		int x = 1;
-		while(x == 1) {
-			Scanner scan = new Scanner(System.in);
-			System.out.println("Please pick an option from below by typing the designated number: ");
-			System.out.println("\n (1): Query1 \n (2): Query2 \n (3): Query3 \n (4): Query4 \n (5): Query5 \n (6): Query6 \n (7): Add \n (8): Update \n (9): Delete \n (10): Exit Program");
-			System.out.println();
-			try {
-				int userInput = scan.nextInt();
-				if(userInput == 1) {
-					System.out.println("You have selected Query 1! Executing results...");
-					//method call for query 1
-				}
-				else if(userInput == 2) {
-					System.out.println("You have selected Query 2! Executing results...");
-					//method call for query 2
-				}
-				else if(userInput == 3) {
-					System.out.println("You have selected Query 3! Executing results...");
-					//method call for query 3
-				}
-				else if(userInput == 4) {
-					System.out.println("You have selected Query 4! Executing results...");
-					//method call for query 4
-				}
-				else if(userInput == 5) {
-					System.out.println("You have selected Query 5! Executing results...");
-					//method call for query 5
-				}
-				else if(userInput == 6) {
-					System.out.println("You have selected Query 6! Executing results...");
-					//method call for query 6
-				}
-				else if(userInput == 7) {
-					System.out.println("You have selected to add an Enemy into the Enemy table. Please wait...");
-					// SEE NOTES BELOW
-					// NOTE: I just started with possibly inputting values above, but change it if youd like. 
-					// NOTE: Also I have not tested this part so idk if it even runs. I just quickly wrote it so we had a start
+		Scanner scan = new Scanner(System.in);
+		int menuSelection;
+		
 
-					/* Scanner enemyScans = new Scanner(System.in);
-					System.out.println("Enemy ID?");
-					int EnemyID = scan.nextInt();
-					System.out.println("Enemy Name?");
-					String EnemyName = scan.nextLine();
-					System.out.println("Enemy HP?");
-					int EnemyHP = scan.nextInt();
-					System.out.println("Enemy Souls?");
-					int EnemySouls = scan.nextInt();
-					System.out.println("Enemy Weakness?");
-					String EnemyWeakness = scan.nextLine();
-					System.out.println("Enemy Resistance?");
-					String EnemyResistance = scan.nextLine();
-					System.out.println("Enemy Immunity?");
-					String EnemyImmunity = scan.nextLine();
-					System.out.println("Enemy Boss (yes/no)?");
-					String EnemyBoss = scan.nextLine();
-					Enemy e = new Enemy(EnemyID, EnemyName, EnemyHP, EnemySouls, EnemyWeakness, EnemyResistance, EnemyImmunity, EnemyBoss);
-					insertEnemy(e);
-					enemyScans.close(); */
-					
-					//add enemy method
-				}
-				else if(userInput == 8) {
-					System.out.println("You have selected to update the Enemy table. Please wait...");
-					//update enemy method
-				}
-				else if(userInput == 9) {
-					System.out.println("You have selected to delete an Enemy from the Enemy table. Please wait...");
-					//delete enemy method
-				}
-				else if(userInput == 10) {
-					System.out.println("Thank you for using Soulscape. :) ");
-					x++;
-					scan.close();
-				}
-				else {
-					System.out.println("Error: Please input one of the specified numbers. ");
-				}
-			} catch(Exception e) {
-				System.out.println("Error: Please input one of the specified numbers. ");
+
+		do{
+			//Print the menu and get selection
+			menuSelection = HandleInput.getMenu(scan);
+
+			//act on whichever menu item the user selected
+			if(menuSelection == 1){
+				
+			}else if(menuSelection == 2){
+
+			}else if(menuSelection == 3){
+
+			}else if(menuSelection == 4){
+
+			}else if(menuSelection == 5){
+
+			}else if(menuSelection == 6){
+
+			}else if(menuSelection == 7){
+
+				scan.nextLine();
+				System.out.print("Enter Enemy Name: ");
+				String name = scan.nextLine();
+				System.out.print("Enter Enemy HP: ");
+				int hp = scan.nextInt();
+				System.out.println();
+				System.out.print("Enter Enemy Souls: ");
+				int souls = scan.nextInt();
+				scan.nextLine();
+				System.out.print("Enter Enemy Weakness: ");
+				String weakness = scan.nextLine();
+				System.out.print("Enter Enemy Resistance: ");
+				String resistance = scan.nextLine();
+				System.out.print("Enter Enemy Immunity: ");
+				String immunity = scan.nextLine();
+				System.out.print("Is this enemy a boss: ");
+				String boss = scan.next();
+
+				Enemy e = new Enemy(name, hp, souls, weakness, resistance, immunity, boss);
+				db.insertEnemy(e);
+
+			}else if(menuSelection == 8){
+				
+			}else if(menuSelection == 9){
+				System.out.print("Insert EnemyID : ");
+				int input = scan.nextInt();
+
+
+				db.deleteEnemy(input);
 			}
-		}
 
+			System.out.println(menuSelection);
+		}while (menuSelection != 10);
 		
 		/* try {
 			String query = "SELECT * FROM Enemy";
